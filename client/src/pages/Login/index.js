f58ce5d18039ab45e7deffda47da71de79d7c6ef
John Herman <hermanjohn2@gmail.com>
2020-11-09 20:29:28 -0500

    Initialized log in view using Material-UI components. Will work on component logic and impletmenting Redux as a global state manager.

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,11 +1,57 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import TextField from '@material-ui/core/TextField';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+	root: {
+		width: '100%',
+		maxWidth: 600,
+		minWidth: 300,
+		margin: 'auto',
+		marginTop: 100
+	},
+	title: {
+		margin: 'auto',
+		fontSize: 20
+	},
+	input: {
+		width: 400,
+		margin: 'auto'
+	},
+	buttonGroup: {
+		margin: 'auto'
+	}
+});
 
 const Login = () => {
+	const classes = useStyles();
+
 	return (
-		<div>
-			<h1>Doggo</h1>
-			<h2>Username:</h2>
-			<h2>Password:</h2>
+		<div className={classes.root}>
+			<Typography variant="h1" gutterBottom>
+				{'{d}'}oggo.
+			</Typography>
+			<Card>
+				<CardContent className={classes.input}>
+					<TextField id="outlined-basic" label="Email" variant="outlined" />
+					<TextField id="outlined-basic" label="Password" variant="outlined" />
+				</CardContent>
+				<CardActions>
+					<ButtonGroup
+						className={classes.buttonGroup}
+						size="small"
+						aria-label="Login and Register button group">
+						<Button>Login</Button>
+						<Button>Register</Button>
+					</ButtonGroup>
+				</CardActions>
+			</Card>
 		</div>
 	);
 };
